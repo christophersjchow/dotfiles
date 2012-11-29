5d2841501b11734f5b6821d463d8ef079632f1d2
Christopher Chow <chris@chowie.net>
2012-11-30 10:34:55 +1100

    Remove non generic config files.
    Make install script link directories too.

--- go.rb
+++ go.rb
@@ -3,11 +3,12 @@
 require 'fileutils'
 
 Dir.chdir File.dirname(__FILE__) do
-  puts "Symlinking..."
-  Dir.glob("*").each do |file|
+  puts 'Symlinking...'
+  Dir.glob('**').each do |file|
     unless %w(go.rb README.md gitconfig.sample).include?(file)
       puts "Symlinking #{file} to #{File.dirname(__FILE__)}/#{file}"
-      FileUtils.ln_s(File.absolute_path(File.join(File.dirname(__FILE__), file)), "#{ENV["HOME"]}/.#{file}", :force => true)
+      FileUtils.ln_s(File.absolute_path(File.join(File.dirname(__FILE__), file)),
+                     "#{ENV["HOME"]}/.#{file}", :force => true)
     end
   end
 end
