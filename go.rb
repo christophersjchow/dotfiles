5b74b5e205e645cb6aecde130715fcae9b9d11ff
Christopher Chow <christopher.chow@kinesis.org>
2013-08-07 12:20:23 +1000

    Include iTerm2 base16-tomorrow colours.

--- go.rb
+++ go.rb
@@ -5,7 +5,7 @@ require 'fileutils'
 Dir.chdir File.dirname(__FILE__) do
   puts 'Symlinking...'
   Dir.glob('**').each do |file|
-    unless %w(go.rb README.md gitconfig.sample).include?(file)
+    unless %w(go.rb README.md gitconfig.sample base16-tomorrow.dark.itermcolors).include?(file)
       puts "Symlinking #{file} to #{File.dirname(__FILE__)}/#{file}"
       FileUtils.ln_s(File.expand_path(File.join(File.dirname(__FILE__), file)),
                      "#{ENV["HOME"]}/.#{file}", :force => true)
