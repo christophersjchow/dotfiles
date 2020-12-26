dfd257f35da00256742911fca28f0558c22fffb7
Christopher Chow <chris@chowie.net>
2020-12-26 15:13:13 +1100

    add hacky install script

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/usr/local/bin/bash
+
+ROOT=$(pwd)
+
+declare -a main_dotfiles
+main_dotfiles=(
+  zlogin
+  zprofile
+  zshenv
+  zshrc
+  zprezto
+  zpreztorc
+  vimrc
+  vim
+  tmux.conf
+  sqliterc
+  screenrc
+  pryrc
+  lscolors
+  lftp
+  hushlogin
+  gvimrc
+  gitexclude
+  gemrc
+  colours
+)
+
+declare -A xdg_config
+xdg_config=(
+  [vim]=nvim
+)
+
+for file in "${main_dotfiles[@]}";
+do
+  ln -nfs "$ROOT/$file" "~/.$file"
+done
+
+for file in "${!xdg_config[@]}";
+do
+  ln -nfs "$ROOT/$file" "~/.config/${xdg_config[$file]}"
+done
