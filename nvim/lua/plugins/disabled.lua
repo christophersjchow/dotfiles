f5dc5621bcd41f2193370b2931141105e7ffc18d
Christopher Chow <chris@chowie.net>
2023-11-25 20:37:56 +1100

    disable lazyvim dashboard

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -9,4 +9,5 @@ return {
   { "ggandor/flit.nvim", enabled = false },
   { "ggandor/leap.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
 }
