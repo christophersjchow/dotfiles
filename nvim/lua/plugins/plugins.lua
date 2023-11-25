54031d07c0b8a18fd531f5ea61ebfccc76ce6861
Christopher Chow <chris@chowie.net>
2023-11-25 20:39:26 +1100

    setup golang support

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -103,6 +103,20 @@ return {
     end,
   },
   {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shfmt",
+        "gopls",
+        "gospel",
+        "gofumpt",
+        "goimports",
+        -- "flake8",
+      },
+    },
+  },
+  {
     "vim-test/vim-test",
     keys = {
       { "<leader>R", ":w<cr> :TestNearest<cr>", desc = "Run nearest test", silent = true },
@@ -131,4 +145,16 @@ return {
   { "vim-ruby/vim-ruby" },
   { "wuelnerdotexe/vim-astro" },
   { "yuezk/vim-js" },
+  {
+    "ray-x/go.nvim",
+    dependencies = {
+      "ray-x/guihua.lua",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
 }
