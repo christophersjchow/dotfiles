514513a77635d0efe76a1fc063bbd36c591bc146
Christopher Chow <cchow@scentregroup.com>
2025-08-04 08:36:00 +1000

    add mise paths for shims

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,7 @@
 vim.lsp.enable("tofu_ls")
 
+-- Prepend mise shims to PATH
+vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
