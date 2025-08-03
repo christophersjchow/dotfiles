dd4b3ed67f72bfde6bcdceb99de0fa5c161c47be
Christopher Chow <chris@chowie.net>
2025-08-03 17:14:59 +1000

    disable rendering markdown in editor

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "folke/todo-comments.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "williamboman/mason-lspconfig.nvim", enabled = false },
+  { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
 }
