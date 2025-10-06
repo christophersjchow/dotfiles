81dd1289eddc1f9439f4b0a689d7ecd0208edb42
Christopher Chow <chris@chowie.net>
2025-10-06 11:18:13 +1100

    update lazyvim config with new plugin names

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
-  { "echasnovski/mini.ai", enabled = false },
+  { "miniai/mini.ai", enabled = false },
   { "folke/todo-comments.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "williamboman/mason-lspconfig.nvim", enabled = false },
+  { "mason-org/mason-lspconfig.nvim", enabled = false },
   { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
 }
