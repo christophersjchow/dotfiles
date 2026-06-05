ae35dbe5b5fa58ce8cfde5ee0235e53ef574e30c
Christopher Chow <cchow@scentregroup.com>
2026-06-05 12:24:09 +1000

    remove workaround for mason version change

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,6 +2,5 @@ return {
   { "miniai/mini.ai", enabled = false },
   { "folke/todo-comments.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "mason-org/mason-lspconfig.nvim", enabled = false },
   { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
 }
